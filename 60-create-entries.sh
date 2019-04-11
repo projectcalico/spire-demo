fd3db29aeac04b4d30cc10a23d80abd195284dc9
Spike Curtis <spike@tigera.io>
2019-04-11 15:43:15 -0700

    Fixup SPIFFE ID entries
    Signed-off-by: Spike Curtis <spike@tigera.io>

--- 60-create-entries.sh
+++ 60-create-entries.sh
@@ -3,5 +3,5 @@ SPIRE='kubectl exec -ti -n spire spire-server-0 -- /opt/spire/bin/spire-server'
 
 $SPIRE entry create -spiffeID spiffe://example.org/k8s -selector k8s_sat:cluster:demo-cluster -node
 $SPIRE entry create -spiffeID spiffe://example.org/ns/default/sa/customer -selector k8s:ns:default -selector k8s:sa:customer -parentID spiffe://example.org/k8s
-$SPIRE entry create -spiffeID spiffe://example.org/ns/default/sa/customer -selector k8s:ns:default -selector k8s:sa:summary -parentID spiffe://example.org/k8s
-$SPIRE entry create -spiffeID spiffe://example.org/ns/default/sa/customer -selector k8s:ns:default -selector k8s:sa:database -parentID spiffe://example.org/k8s
+$SPIRE entry create -spiffeID spiffe://example.org/ns/default/sa/summary -selector k8s:ns:default -selector k8s:sa:summary -parentID spiffe://example.org/k8s
+$SPIRE entry create -spiffeID spiffe://example.org/ns/default/sa/database -selector k8s:ns:default -selector k8s:sa:database -parentID spiffe://example.org/k8s
