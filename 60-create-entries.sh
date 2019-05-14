1549f5aa47a87afd10186c2f3d72f49b613b8040
Shaun Crampton <shaun@tigera.io>
2019-05-14 11:53:35 +0100

    Fix whitespace

--- 60-create-entries.sh
+++ 60-create-entries.sh
@@ -5,3 +5,4 @@ $SPIRE entry create -spiffeID spiffe://example.org/k8s -selector k8s_sat:cluster
 $SPIRE entry create -spiffeID spiffe://example.org/ns/default/sa/customer -selector k8s:ns:default -selector k8s:sa:customer -parentID spiffe://example.org/k8s
 $SPIRE entry create -spiffeID spiffe://example.org/ns/default/sa/summary -selector k8s:ns:default -selector k8s:sa:summary -parentID spiffe://example.org/k8s
 $SPIRE entry create -spiffeID spiffe://example.org/ns/default/sa/database -selector k8s:ns:default -selector k8s:sa:database -parentID spiffe://example.org/k8s
+
